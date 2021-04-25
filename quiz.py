1c3a00cd48a92928a58219243345b885812b0ed9
felixkan <felixkkan@gmail.com>
2021-04-25 00:40:44 -0400

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -2,79 +2,93 @@
 """
 Created on Sun Mar  7 18:07:26 2021
 
-# streamlit run draft_quiz.py
+# streamlit run quiz.py
 """
+
 
 import streamlit as st
 import time
 
 """
 # Emotion Quiz
-Here's our first attempt to create a quiz!
+Get ready to answer these questions and you'll see what is your emotion!'
 """
 
 scores = []
 
-options = ['A: purple', 'B: green',  'C: red',  'D: blue']
-points = [20, 5, 10, 30]
+options = ['A: Pizza', 'B: Hot Dog',  'C: Curry',  'D: Bun']
+points = [10, 30, 5, 20]
 score_dict = dict(zip(options, points))
 ans = st.selectbox(
         """
-        Q1: What is your favourite colour? 
+        Q1: What do you like to eat? 
         """,
         options)
 scores.append(score_dict[ans])
-# 'You selected: ', ans
-# st.write( f"You got {score_dict[ans]} points." )
 
-options = ['A: ermine', 'B: dolphin',  'C: red panda',  'D: koala']
-points = [10, 30, 20, 5]
+options = ['A: Coca cola', 'B: Canada Dry',  'C: Nestea',  'D: Sprite']
+points = [30, 5, 20, 10]
 score_dict = dict(zip(options, points))
 ans = st.selectbox(
         """
-        Q2: What is your favourite animal? 
+        Q2: Choose a kind of pop. 
         """,
         options)
 scores.append(score_dict[ans])
-# st.write( f"You got {score_dict[ans]} points." )
 
-options = ['A: badminton', 'B: tennis',  'C: basketball',  'D: running']
-points = [10, 30, 20, 5]
+options = ['A: Basketball', 'B: Soccer',  'C: Badminton',  'D: Tennis']
+points = [20, 5, 30, 10]
 score_dict = dict(zip(options, points))
 ans = st.selectbox(
         """
-        Q3: What is your best sports? 
+        Q3: Which sport is most interesting to you? 
         """,
         options)
 scores.append(score_dict[ans])
-# st.write( f"You got {score_dict[ans]} points." )
 
+
+options = ['A: Purple', 'B: Green',  'C: Red',  'D: Blue']
+points = [10, 30, 5 ,20 ]
+score_dict = dict(zip(options, points))
+ans = st.selectbox(
+        """
+        Q4: Choose a colour. 
+        """,
+        options)
+scores.append(score_dict[ans])
+
+options = ['A: Fall', 'B: Spring',  'C: Summer',  'D: Winter']
+points = [5, 20, 30 ,10 ]
+score_dict = dict(zip(options, points))
+ans = st.selectbox(
+        """
+        Q5: Which season do you like? 
+        """,
+        options)
+scores.append(score_dict[ans])
 # -------------------- #
 
 # Calculate the total score and output the result.
 total_score = sum(scores)
 
-if 0 <= total_score <= 100:
+if 130 <= total_score <= 150:
     result = "angry"
-elif 101 <= total_score <= 150:
+elif 51 <= total_score <= 100:
     result = "calm"
-elif 151 <= total_score <= 200:
+elif 0 <= total_score <= 50:
     result = "sad"
-elif 201 <= total_score <= 250:
+elif 101 <= total_score <= 130:
     result = "happy"
 else:
-    result = "unknown"
+    result = "What in the whole wide world?! You didn't pass the quiz!"
 
-# print(f"You got {total_score} points in total.")
-# print(f"You are {result} !!")
 
+# Output the results.
 left_column, right_column = st.beta_columns(2)
-pressed = right_column.button('Done!')
+pressed = right_column.button('See what is your emotion!')
 if pressed:
-    # right_column.write(f"You got {total_score} points in total.")
-    # right_column.write(f"You are {result}!!")
-    f"""## You are {result}!!   
-    You got {total_score} points in total."""
+    f"""## You are {result}!! Good job!   
+    You got {total_score} points in total.Thank you for doing the quiz!"""
     
 
 
